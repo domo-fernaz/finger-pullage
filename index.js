d3e38d7ff0914a710563ee4b35509d4e27173cd5
domo-fernaz <97562935+domo-fernaz@users.noreply.github.com>
2022-03-30 12:57:18 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let btn = document.getElementById("fard");
 let countElement = document.getElementById("count");
 let count = 0;
 
-btn.onClick = function() {
+btn.onclick = function() {
   count++;
+  alert("*fart*");
   countElement.innerHTML = `You fell for it ${count} times`;
 }
